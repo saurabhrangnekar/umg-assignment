6bb501c979f453006d91dde60444ea00b7a606d4
saurabhrangnekar <saurabh.rangnekar@gmail.com>
2022-04-25 14:40:21 -0700

    Added SQL script

---
+++ scripts/umg-dbscript.sql
@@ -0,0 +1,37 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               5.7.21 - MySQL Community Server (GPL)
+-- Server OS:                    Win32
+-- HeidiSQL Version:             11.2.0.6213
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+-- Dumping database structure for umg-assignment
+CREATE DATABASE IF NOT EXISTS `umg-assignment` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `umg-assignment`;
+
+-- Dumping structure for table umg-assignment.track_master
+DROP TABLE IF EXISTS `track_master`;
+CREATE TABLE IF NOT EXISTS `track_master` (
+  `isrc` varchar(50) NOT NULL,
+  `name` varchar(500) DEFAULT NULL,
+  `duration` int(11) DEFAULT NULL,
+  `explicit` tinyint(1) DEFAULT NULL,
+  `created_on` datetime DEFAULT CURRENT_TIMESTAMP,
+  `modified_on` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`isrc`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Data exporting was unselected.
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
